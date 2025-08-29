413440eeded7d4b563bf30a92d904b02e77613cc
Kevin Manrique Campos Granados <USSS027225@ugb.edu.sv>
2025-08-29 13:08:05 -0600

    Mismos calculos usando LINQ  en todo

---
+++ ESTADISTICA2025/ESTADISTICA2025/E4.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ESTADISTICA2025
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Matrices
+        //Funciones
+        //LINQ = Lenguaje de Consultas Integrado
+
+        double media(int[] serie) {
+            return serie.Average();
+        }
+
+        double tipica(int[] serie, double m) { 
+       return Math.Sqrt(serie.Average(n=>Math.Pow(n-m,2) ));
+        }
+
+        double armonica(int[] serie) {
+            int n = serie.Length;
+            return n / serie.Sum(num => 1.0 /num);
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e) {
+            int[] serie = txtSerie.Text.Split(',').Select(n=>int.Parse(n)).ToArray(); //5,8,1,9 => ["5", "8", "1", "9"]
+
+            double mediaAritmetica = media(serie);
+            double desviacionTipica = tipica(serie, mediaAritmetica);
+            double mediaArmonica = armonica(serie);
+
+            lblMedia.Text = "MEDIA: " + mediaAritmetica.ToString();
+            lblTipica.Text = "TIPICA: " + desviacionTipica.ToString();
+            lblArmonica.Text = "ARMONICA: " + mediaArmonica.ToString();
+        }
+    }
+}
