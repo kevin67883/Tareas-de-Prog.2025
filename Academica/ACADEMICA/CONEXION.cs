c9723d11f8e0bf971af4f27b67d45ca1b73718b8
Kevin Manrique Campos Granados <USSS027225@ugb.edu.sv>
2025-09-14 22:23:32 -0600

    Creacion de la BD y la clase conexion.

---
+++ Academica/ACADEMICA/CONEXION.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;//Esta es la libreria que me permite usar comandos para trabajar con base de datos
+using System.Data.SqlClient;//Esta libreria me permite trabajar con SQL Server
+
+namespace ACADEMICA
+{
+
+    internal class CONEXION
+    {
+        //Definir los miembros de la clase, atributos y metodos.
+        SqlConnection objConexion = new SqlConnection();//Conectarse a la BD.
+        SqlCommand objComando = new SqlCommand();//Ejecutar SQL en la BD: Lectura, Actualizacion, Elliminacion, Insercion.
+        SqlDataAdapter objAdaptador = new SqlDataAdapter();//un puente entre la BD y lla aplicacion.
+        DataSet objDs = new DataSet();//Es una representacion de la arquitectura de la BD en memoria.
+
+        public CONEXION()
+        { //Constructor. inicializador de los atributos
+            String cadenaConexion = "";
+            objConexion.ConnectionString = cadenaConexion;
+            objConexion.Open();//Abrir la conexion a la BD
+
+
+        }
+        public DataSet obtenerDatos() {
+            objDs.Clear();//Limpiar el dataset
+            objComando.Connection = objConexion;//Establecer la conexion para ejecutar los comandos
+
+            objAdaptador.SelectCommand = objComando;//Establecer el comando de seleccion
+            objComando.CommandText = "SELECT * FROM alumnos";
+            objAdaptador.Fill(objDs, "alumnos");//Tomando los datos de la BD y llenando el DataSet
+
+            return objDs;
+        }
+
+    }
+}
