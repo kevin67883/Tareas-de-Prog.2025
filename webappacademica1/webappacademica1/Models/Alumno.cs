9303d867c0e52b6458394421b869e34a1eb10890
Kevin Manrique Campos Granados <USSS027225@ugb.edu.sv>
2025-10-15 09:13:37 -0600

    Creacion de la rama webapp y del proyecto web app academica, confiuracion de la clase MyDbContext y Alumno y la configuracion de la cadena de conexion a la BD

---
+++ webappacademica1/webappacademica1/Models/Alumno.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webappacademica1.Models{
+    
+    public class Alumno{
+        [Key]
+        public int idAlumno { get; set; }
+        public int codigo { get; set; }
+        public int nombre { get; set; }
+        public int direccion { get; set; }
+        public int telefono { get; set; }
+    }
+}
